0d67ab77a0dc38917c1d261f61b8fdf83fbdfacf
Matteo Vicenzi <matteo.vicenzi7@gmail.com>
2023-04-11 14:58:49 -0500

    fix indenting

--- src/SteppingAction.cc
+++ src/SteppingAction.cc
@@ -25,17 +25,16 @@ void SteppingAction::UserSteppingAction(const G4Step* aStep) {
   
   if( volume==nullptr ) aTrack->SetTrackStatus(G4TrackStatus::fStopAndKill);
   else if( volume->GetName() == "world" ){       
+    G4double endFLArE = 4 * m;
+    G4double totYokeX = 4.5 * m;
+    G4double totYokeY = 3 *m;
 	
-	G4double endFLArE = 4 * m;
-	G4double totYokeX = 4.5 * m;
-	G4double totYokeY = 3 *m;
-	
-	//if before end of FLArE, kill
-	if( post_pos.z() < endFLArE ) aTrack->SetTrackStatus(G4TrackStatus::fStopAndKill); 
-	
-	//if after enf of FLArE, kill if outside FASER magnet cross-section
-	else if ( TMath::Abs(post_pos.x()) > totYokeX/2. || TMath::Abs(post_pos.y()) > totYokeY/2. )
-		aTrack->SetTrackStatus(G4TrackStatus::fStopAndKill);
+    //if before end of FLArE, kill
+    if( post_pos.z() < endFLArE ) aTrack->SetTrackStatus(G4TrackStatus::fStopAndKill); 
+    
+    //if after enf of FLArE, kill if outside FASER magnet cross-section
+    else if ( TMath::Abs(post_pos.x()) > totYokeX/2. || TMath::Abs(post_pos.y()) > totYokeY/2. )
+      aTrack->SetTrackStatus(G4TrackStatus::fStopAndKill);
   }
   
   /*// if the track is out of the active volumes, kill this track
@@ -74,24 +73,28 @@ void SteppingAction::UserSteppingAction(const G4Step* aStep) {
 
 void SteppingAction::TrackLiveDebugging(const G4Step* step){
 
-	G4Track* track = step->GetTrack();
+  G4Track* track = step->GetTrack();
 
-	if( track->GetTrackStatus() != fAlive && track->GetTrackStatus() != fStopButAlive) return;
+  if( track->GetTrackStatus() != fAlive && track->GetTrackStatus() != fStopButAlive) return;
 
-	G4LogicalVolume* volume = step->GetPreStepPoint()->GetTouchableHandle()->GetVolume()->GetLogicalVolume();
-	G4LogicalVolume* volume_after = step->GetPostStepPoint()->GetTouchableHandle()->GetVolume()->GetLogicalVolume();
+  G4LogicalVolume* volume = step->GetPreStepPoint()->GetTouchableHandle()->GetVolume()->GetLogicalVolume();
+  G4LogicalVolume* volume_after = step->GetPostStepPoint()->GetTouchableHandle()->GetVolume()->GetLogicalVolume();
 
-	G4ThreeVector pre_pos = step->GetPostStepPoint()->GetPosition();
-	G4ThreeVector post_pos = step->GetPostStepPoint()->GetPosition();
-	G4double edep = step->GetTotalEnergyDeposit();
+  G4ThreeVector pre_pos = step->GetPreStepPoint()->GetPosition();
+  G4ThreeVector post_pos = step->GetPostStepPoint()->GetPosition();
+  G4ThreeVector pre_mom = step->GetPreStepPoint()->GetMomentum();
+  G4ThreeVector post_mom = step->GetPostStepPoint()->GetMomentum();
+  G4double edep = step->GetTotalEnergyDeposit();
 	 
-	int PDG = track->GetParticleDefinition()->GetPDGEncoding();
-	G4String ParticleName = track->GetDynamicParticle()->GetParticleDefinition()->GetParticleName();
-	int TID = track->GetTrackID();
-	int SID = step->GetTrack()->GetCurrentStepNumber();
-
-	std::cout << "Track " << TID << " - " << "PDG " << PDG << " " << ParticleName << std::endl;
-	std::cout << "stepping... " << SID << " edep" << edep << std::endl;
-	std::cout << "(" << pre_pos.x() << "," << pre_pos.y() << "," << pre_pos.z() << ") in " << volume->GetName();
-	std::cout << " ---> "  << "(" << post_pos.x() << "," << post_pos.y() << "," << post_pos.z() << ") in " << volume_after->GetName() << std::endl;
+  int PDG = track->GetParticleDefinition()->GetPDGEncoding();
+  G4String ParticleName = track->GetDynamicParticle()->GetParticleDefinition()->GetParticleName();
+  int TID = track->GetTrackID();
+  int SID = step->GetTrack()->GetCurrentStepNumber();
+  
+  std::cout << "Track " << TID << " - " << "PDG " << PDG << " " << ParticleName << std::endl;
+  std::cout << "stepping... " << SID << " edep" << edep << std::endl;
+  std::cout << "(" << pre_pos.x() << "," << pre_pos.y() << "," << pre_pos.z() << ") in " << volume->GetName();
+  std::cout << " ---> "  << "(" << post_pos.x() << "," << post_pos.y() << "," << post_pos.z() << ") in " << volume_after->GetName() << std::endl;
+  std::cout << "momentum: (" << pre_mom.x() << "," << pre_mom.y() << "," << pre_mom.z() << ") ---> (";
+  std::cout << post_mom.x() << "," << post_mom.y() << "," << post_mom.z() << ")" << std::endl;
 }
